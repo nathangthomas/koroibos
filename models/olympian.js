1ac62429830e64f023bdca68511038483425d1a4
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-01-11 13:00:37 -0700

    Add olympian model

---
+++ models/olympian.js
@@ -0,0 +1,13 @@
+class Olympian {
+  constructor(obj) {
+    this.name = obj.name,
+    this.sex = obj.sex,
+    this.age = obj.age,
+    this.height = obj.height,
+    this.weight = obj.weight,
+    this.sport = obj.sport,
+    this.team = obj.team
+  }
+
+
+module.exports = Olympian;
