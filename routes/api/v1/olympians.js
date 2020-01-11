214aece2fb71085dccffb83b6246926af72f4e89
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-01-11 13:25:29 -0700

    Add olympians function

--- routes/api/v1/olympians.js
+++ routes/api/v1/olympians.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const Olympian = require("../../../models/olympian");
+
+//database connection
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 
 router.get('/', (req, res) => {
-  res.send('Testing testing one two three... ')
+  olympians()
+  .then olympians => {
+    if (olympians.length){
+      res.status(200).send(olympians)
+    } else {
+      res.status(404).json({
+        error: 'Not found.'
+      })
+    }
+  }
+  .catch(error => res.status(500).send({ error }))
 });
+
+
+async function olympians() {
+  try{
+    return await database('nathanthomas')
+      .conlmn(['name', 'team', 'age', 'sport'])
+  } catch(e) {
+    return e;
+  }
+}
 
 module.exports = router;
