3c29f624eb086456b6c3ab94575a2c3b66227be5
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-01-13 10:02:10 -0700

    Refactor olympianStats function

--- routes/api/v1/olympian_stats.js
+++ routes/api/v1/olympian_stats.js
@@ -25,21 +25,23 @@ async function olympianStats() {
   const exposureData = {}
     await totalNumOfOlympians()
       .then(totalNum => {
-        exposureData['total_competing_olympians:'] = totalNum
+        exposureData['"total_competing_olympians"'] = totalNum
       })
-    exposureData['average_weight:'] = {}
-    exposureData['average_weight:']['unit'] = 'kg'
+    exposureData['"average_weight"'] = {}
+    exposureData['"average_weight"']['"unit"'] = 'kg'
     await averageMaleWeight()
       .then(avgM => {
-        exposureData['average_weight:']['male_olympians'] = avgM
+        console.log(avgM)
+        exposureData['"average_weight"']['"male_olympians"'] = avgM
       })
     await averageFemaleWeight()
       .then(avgF => {
-        exposureData['average_weight:']['female_olympians'] = avgF
+        console.log(avgF)
+        exposureData['"average_weight"']['"female_olympians"'] = avgF
       })
     await averageAge()
       .then(age => {
-        exposureData['average_age:'] = age
+        exposureData['"average_age"'] = age
       })
     return exposureData
 }
@@ -52,38 +54,39 @@ async function totalNumOfOlympians() {
 
 async function averageMaleWeight() {
   let totalWeight = 0
-  var men = await database('olympics_data').where('sex', '=', 'M').whereNotNull('weight').select('name').groupBy('name')
+  var men = await database('olympics_data').where('sex', '=', 'M').whereNotNull('weight').select('weight').groupBy('weight')
   await Promise.all(men.map(async (m) => {
     var weight = parseInt(m.weight)
     totalWeight += weight
   }))
   var numOfMen = men.length
   var averageWeight = totalWeight/numOfMen
-  return averageWeight
+  return parseFloat(averageWeight.toFixed(1));
+
 }
 
 async function averageFemaleWeight() {
   let totalWeight = 0
-  var women = await database('olympics_data').where('sex', '=', 'F').whereNotNull('weight').select('name').groupBy('name')
+  var women = await database('olympics_data').where('sex', '=', 'F').whereNotNull('weight').select('weight').groupBy('weight')
   await Promise.all(women.map(async (f) => {
     var weight = parseInt(f.weight)
     totalWeight += weight
   }))
   var numOfWomen = women.length
-  var averageWeight = totalWeight/numOfWomen
-  return averageWeight
+  var averageWeight = (totalWeight/numOfWomen)
+  return parseFloat(averageWeight.toFixed(1));
 }
 
 async function averageAge() {
   var totalAge = 0
-  const allOlympians = await database('olympics_data').select('name').groupBy('name')
+  const allOlympians = await database('olympics_data').select('age').groupBy('age')
   await Promise.all(allOlympians.map(async (olympian) => {
     var age = parseInt(olympian.age)
     totalAge += age
   }))
 var numOfOlympians = allOlympians.length
-var averageAge = totalAge/numOfOlympians
-return averageAge
+var averageAge = (totalAge/numOfOlympians)
+return parseFloat(averageAge.toFixed(1));
 }
 
 
